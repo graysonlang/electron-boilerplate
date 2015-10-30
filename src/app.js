5260e41da96429658834185f01968edb904c4104
Grayson Lang <grayson1@adobe.com>
2015-10-30 16:54:12 -0700

    ES6-ifying release task.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 
 // Modules which you authored in this project are intended to be
 // imported through new ES6 syntax.
-import { greet } from './hello_world/hello_world';
+import { greet } from './hello_world/hello_world'
 
 // Node.js modules and those from npm
 // are required the same way as always.
@@ -13,7 +13,7 @@ var jetpack = require('fs-jetpack').cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
-console.log(jetpack.read('package.json', 'json'));
+console.log(jetpack.read('manifest.json', 'json'));
 
 // window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
